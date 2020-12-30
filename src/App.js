8248692a22c305cef9039ebdfc7efd7251ee9756
Anand Singh <anand4carcility@gmail.com>
2020-12-30 06:15:21 +0530

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((data) => data.json())
+      .then((user) =>
+        this.setState({
+          monsters: user,
+          searchText: "",
+        })
+      );
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      // setState is an asynchronous call and immidiately accessing
+      // the this.state will not reflect the values
+      // to do so you can use the callback function of
+      // setState to see the updated value
+      searchText: e.target.value,
+    });
+  };
+
+  render() {
+    // Filter the monsters array based on the searchField
+    // Destructuring
+    const { monsters, searchText } = this.state;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          onHandleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
